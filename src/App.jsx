a4beabb151a81914caaef05339e0c0c82e9c2909
arensin8 <arensinani4@gmail.com>
2025-03-26 09:37:40 -0700

    creating the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import {BrowserRouter} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Tech from './components/Tech';
+import Works from './components/Works';
+import StarsCanvas from './components/canvas/Stars';
+
 const App = () => {
   return (
-    <div>
-      3d portolio website
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary '>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <div className='relative z-0'>
+        <Contact/>
+        <StarsCanvas />
+      </div>
     </div>
+    </BrowserRouter>
   );
 };
 
