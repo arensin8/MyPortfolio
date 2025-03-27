e7d4aae8dbf8cdbc3411c434714a3d50981d065d
arensin8 <arensinani4@gmail.com>
2025-03-27 11:25:34 -0700

    creating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { styles } from '../styles';
+import { Link } from 'react-router-dom';
+import { logo , menu , close} from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
+
+  const [active,setActive] = useState(" ")
+  const [toggle,setToggle] = useState(false)
+
   return (
-    <div>
-      Navbar
+    <nav className={
+      `${styles.paddingX} w-full flex items-center py-5 
+      fixed top-0 z-20 bg-primary`
+    }> 
+    <div className='w-full flex justify-between 
+    items-center max-w-7xl mx-auto'>
+    <Link 
+      to='/' 
+      className='flex items-center gap-2'
+      onClick={() => { 
+        setActive("")
+        window.scrollTo(0,0)
+      }}
+      >
+        <img src={logo} alt="logo" className='w-9 
+        h-9 object-contain'/>
+        <p
+        className='text-white text-[18px] font-bold cursor-pointer'>
+          Aren Sinaei <span className='sm:block hidden'>| Portfolio</span></p>
+      </Link>
+      <ul className='list-none hidden sm:flex flex-row gap-10'>
+        {navLinks.map((link) => (
+          <li key={link.id} 
+          className={`
+          ${active === link.title ? "text-white"
+          :"text-secondary"} hover:text-white text-[18px] fobt-medium curspor-pointer`}
+          onClick={() => setActive(link.title)}
+          >
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img 
+        src={toggle ? close : menu} 
+        alt="menu" 
+        className='w-[28px] h-[28px] cursor-pointer object-contain'
+        onClick={() => setToggle(!toggle)}/>
+      </div>
+
+      <div className={
+        `${!toggle ? 'hidden' : 'flex'} p-6 absolute black-gradient right-0
+        top-20 mx-4 my-2 z-10 min-w-[140px] rounded-xl`}></div>
     </div>
+    </nav>
+
   );
 };
 
