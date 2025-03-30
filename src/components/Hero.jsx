6a948b1ac5d43abb433c75eef2344e4733300adf
arensin8 <arensinani4@gmail.com>
2025-03-30 14:06:51 -0700

    finishing the hero section and also make it responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} 
-      absolute flex flex-row top-[120px] max-w-7xl mx-auto
+      absolute inset-0 flex flex-row top-[120px] max-w-7xl mx-auto
       items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
@@ -21,9 +21,32 @@ const Hero = () => {
           I develop modern web applications <br className='sm:block hiddens'/> and user interfaces.
           </p>
         </div>
-
       </div>
-        <ComputersCanvas />
+
+      <ComputersCanvas />
+
+      <div 
+      className='absolute xs:bottom-10 bottom-32
+      flex w-full justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] 
+          rounded-3xl justify-center items-start p-2
+          flex border-4 border-secondary'>
+            <motion.div
+            animate={{
+              y : [0,24,0]
+            }}
+            transition={{
+              duration : 1.5,
+              repeat : Infinity,
+              repeatType : 'loop'
+            }}
+            className='w-3 h-3 rounded-full bg-secondary mb-1'>
+              
+            </motion.div>
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
