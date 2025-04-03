175523f9c6fe2270d6a01e5bc9ae596dd813e757
arensin8 <arensinani4@gmail.com>
2025-04-02 18:28:36 -0700

    Finishing the experiences section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,23 +9,40 @@ import { textVariant } from '../utils/motion';
 
 const ExperienceCard = ({experience}) => (
   <VerticalTimelineElement
-  contentStyle={{background:"#1d1836" , color : "#fff"}}
-  contentArrowStyle={{borderRight : "7px solid #232631"}}
-  date={experience.date}
-  iconStyle={{background : experience.iconBg}}
-  icon={
-    <div>
-      <img 
-        src={experience.icon} 
-        alt={experience.company_name}
-        className='h-[60%] w-[60%] object-contain' />
-    </div>
-  }>
+    contentStyle={{background:"#1d1836" , color : "#fff"}}
+    contentArrowStyle={{borderRight : "7px solid #232631"}}
+    date={experience.date}
+    iconStyle={{background : experience.iconBg}}
+    icon={
+      <div 
+        className='flex justify-center items-center h-full w-full'>
+        {/* <img 
+          src={experience.icon} 
+          alt={experience.company_name}
+          className='h-[60%] w-[60%] object-contain' /> */}
+      </div>
+    }>
     <div>
       <h3 className='text-white text-[24px] font-bold'>
         {experience.title}
       </h3>
+      <p className='text-secondary font-[16px] font-semibold'
+      style={{margin : 0 }}>
+        {experience.company_name}
+      </p>
     </div>
+
+    <ul className='mt-5 ml-5 list-disc space-y-2'>
+      {experience.points.map((point , index) => (
+        <li
+          key={`experience-point${index}`}
+          className='text-white-100 text-[14px] 
+          pl-1 tracking-wider'>
+          {point}
+        </li>
+          ))}
+      
+    </ul>
   </VerticalTimelineElement>
 )
 
@@ -40,9 +57,9 @@ const Experience = () => {
 
     <div className='mt-20 flex flex-col'>
       <VerticalTimeline>
-        {experiences.map((experience, index) => {
-          <ExperienceCard key={index} experience={experience}/>
-        })}
+      {experiences.map((experience, index) => (
+        <ExperienceCard key={index} experience={experience}/>
+      ))}
       </VerticalTimeline>
     </div>
     </>
