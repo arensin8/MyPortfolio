f636e013036821562c3a2523c136d8fe03fed671
arensin8 <arensinani4@gmail.com>
2025-04-07 10:46:40 -0700

    Starting contact  section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import {useState,useRef} from 'react';
+import {motion} from 'framer-motion'
+import {styles} from '../styles'
+import {EarthCanvas} from './canvas'
+import emailjs from '@emailjs/browser'
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+
+  const formRef = useRef();
+  const [form , setForm] = useState({
+    name : '',
+    email: '',
+    message : ''
+  })
+
+  const [loading , setLoading] = useState(false);
+
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {}
+
+
+
   return (
-    <div>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse
+    flex gap-10 overflow-hidden'>
       Contact
     </div>
   );
 };
 
-export default Contact;
+export default SectionWrapper(Contact , 'contact');
