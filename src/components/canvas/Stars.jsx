58df8840bf851996f5aeac149b9b57b54af380d0
arensin8 <arensinani4@gmail.com>
2025-04-10 18:08:31 -0700

    creating stars canvas section

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,23 +1,32 @@
-import { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { useState , useRef , Suspense } from "react";
+import { Canvas , useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = (props) => {
-  const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
-  useFrame((state, delta) => {
+  const ref = useRef();
+  //creating sphears
+  const sphere = random.inSphere(new Float32Array(5000) , {radius : 1.2})
+
+  //for stars rotationing
+  useFrame((state,delta) => {
     ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+    ref.current.rotation.y -= delta /15;
+  })
 
   return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-        <PointMaterial
+    <group rotation={[0 , 0, Math.PI / 4]}>
+      <Points 
+        ref={ref} 
+        positions={sphere}
+        stride={3}
+        frustumCulled
+        {...props}
+      >
+        <PointMaterial 
           transparent
-          color='#f272c8'
+          color='f272c8'
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
@@ -29,16 +38,16 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className='w-full h-auto absolute inset-0 z-[-1]'>
-      <Canvas camera={{ position: [0, 0, 1] }}>
+    <div className="absolute h-auto w-full inset-0 z-[-1]">
+      <Canvas camera={{position : [0,0,1]}}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
 
-        <Preload all />
+        <Preload all/>
       </Canvas>
     </div>
-  );
-};
+  )
+}
 
 export default StarsCanvas;
